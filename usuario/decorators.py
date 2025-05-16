52746d3dcd668793203f7a2f9ed8df94805a16b9
gabriel <gabrielmfer127@gmail.com>
2025-05-16 17:21:28 -0300

    :sparkles: feat: controle de acesso redirecionando para tela de acesso negado

--- usuario/decorators.py
+++ usuario/decorators.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponseForbidden
 from functools import wraps
+from django.shortcuts import render
 
 def somente_master(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         if request.user.is_authenticated and request.user.tipo_acesso == 'master':
             return view_func(request, *args, **kwargs)
-        return HttpResponseForbidden("<h2>Acesso restrito a usuários master.</h2>")
+        return render(request, 'acesso_negado.html', status=403)
     return _wrapped_view
