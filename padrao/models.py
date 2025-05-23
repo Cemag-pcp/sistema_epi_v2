deb70af1b5144a7a29570e5165864e21ea0695f5
SaulSantos1 <saulsantos111@gmail.com>
2025-05-23 17:03:02 -0300

    :sparkles: feat: Iniciando os Padroes

--- padrao/models.py
+++ padrao/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from usuario.models import Setor
+from equipamento.models import Equipamento
 
 
 class Padrao(models.Model):
-    cargo = models.CharField(max_length=150)
+    nome = models.CharField(max_length=150)
     setor = models.ForeignKey(Setor, on_delete=models.CASCADE, related_name='setor_padrao')
-    itens = models.JSONField()
+    itens = models.ManyToManyField(Equipamento, related_name='padroes')
     ativo = models.BooleanField(default=True)
     data_criacao = models.DateTimeField(auto_now_add=True)
     data_atualizacao = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.cargo} - {self.setor}'
+        return f'{self.nome} - {self.setor}'
