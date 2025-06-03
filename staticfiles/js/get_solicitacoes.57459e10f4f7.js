ba95863418fcf3b85b0a7903eca1bb14e46853dc
SaulSantos1 <saulsantos111@gmail.com>
2025-06-03 15:51:33 -0300

    STATICFILES

---
+++ staticfiles/js/get_solicitacoes.57459e10f4f7.js
@@ -0,0 +1,45 @@
+export function loadFormDataRequest() {
+
+    const form = document.getElementById("form-card-solict");
+    const spinner = document.getElementById("spinner");
+    form.style.display = 'none';
+    spinner.style.display = 'block';
+
+    fetch('/solicitacao/api')
+        .then(response => response.json())
+        .then(data => {
+            // Preencher equipamentos
+            const equipamentoSelect = document.querySelector('.equipamento');
+            data.equipamentos.forEach(equipamento => {
+                const option = document.createElement('option');
+                option.value = equipamento.id;
+                option.textContent = `${equipamento.codigo} - ${equipamento.nome}`;
+                equipamentoSelect.appendChild(option);
+            });
+
+            // Preencher funcionários
+            const funcionarioSelect = document.querySelector('.funcionario');
+            data.funcionarios.forEach(funcionario => {
+                const option = document.createElement('option');
+                option.value = funcionario.id;
+                option.textContent = `${funcionario.matricula} - ${funcionario.nome}`;
+                funcionarioSelect.appendChild(option);
+            });
+
+            // Preencher motivos
+            const motivoSelect = document.querySelector('.motivo');
+            data.motivo.forEach(motivo => {
+                const option = document.createElement('option');
+                option.value = motivo.id;
+                option.textContent = motivo.nome;
+                motivoSelect.appendChild(option);
+            });
+        })
+        .catch(error => {
+            console.error('Erro ao carregar dados:', error);
+        })
+        .finally(finnaly => {
+            form.style.display = 'block';
+            spinner.style.display = 'none';
+        })
+}
