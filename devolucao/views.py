78e5be9a45c19af876a5be5ab616eb260373a49c
gabriel <gabrielmfer127@gmail.com>
2025-07-11 10:44:17 -0300

    :sparkles: feat: atribuição da quantidade de item a ser devolvido na pagina de devolucao

--- devolucao/views.py
+++ devolucao/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.db import transaction, IntegrityError
+from django.db.models import Sum, F, Q
 from usuario.decorators import somente_master
 from .models import Devolucao, DadosSolicitacao
 import json
@@ -18,7 +19,7 @@ def devolucao(request):
         try:
             data = json.loads(request.body)
             print(data)
-            
+            # return "ok"
             #validar
             required_fields = ['funcionarioId', 'items']
 
@@ -34,17 +35,33 @@ def devolucao(request):
             
             # Como a lista de dicionarios items puxa um DadosSolicitacao diferente, 
             # é possível criar uma devolucao exclusiva para cada elemento dessa lista baseado no id que é o id de DadosSolicitacao
-
             #criar uma devolução
             with transaction.atomic():
                 for item in data['items']:
-                    dados_solicitacao = DadosSolicitacao.objects.get(pk=item['id'])
+                    dados_solicitacao = DadosSolicitacao.objects.annotate(
+                        total_devolvido=Sum('dados_solicitacao_devolucao__quantidade_devolvida')
+                    ).get(pk=item['id'])
+
+                    # Validando o que já foi devolvido
+                    total_ja_devolvido = dados_solicitacao.total_devolvido or 0
+
+                    # Pegando a quantidade disponível para devolução
+                    quantidade_disponivel = dados_solicitacao.quantidade - total_ja_devolvido
+
+                    # Validando a quantidade devolvida
+                    if int(item['qtdDevolvida']) <= 0:
+                        raise ValueError("Quantidade devolvida deve ser maior que zero!")
+                    # Quantidade devolvida não pode ser maior que a quantidade disponível
+                    if int(item['qtdDevolvida']) > quantidade_disponivel:
+                        raise ValueError(f"Quantidade devolvida ({item['qtdDevolvida']}) maior que a disponível ({quantidade_disponivel})!")
+                    # dados_solicitacao = DadosSolicitacao.objects.get(pk=item['id'])
 
                     devolut = Devolucao(
                         dados_solicitacao=dados_solicitacao,
                         responsavel_recebimento=request.user,
                         estado_item=item['condicao'],
                         observacoes=item['observacao'],
+                        quantidade_devolvida=item['qtdDevolvida'],
                     )
                     #salvar
                     devolut.save()
