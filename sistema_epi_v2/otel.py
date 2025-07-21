fcaecadef2c71ef432c017c26b36689fec3fe097
SaulSantos1 <saulsantos111@gmail.com>
2025-07-21 08:31:06 -0300

    Comentando OpenTelemetry

--- sistema_epi_v2/otel.py
+++ sistema_epi_v2/otel.py
@@ -1,20 +1,20 @@
-from opentelemetry import metrics
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
-from opentelemetry.exporter.prometheus import PrometheusMetricReader
-from opentelemetry.instrumentation.django import DjangoInstrumentor
-from opentelemetry.sdk.resources import Resource
+# from opentelemetry import metrics
+# from opentelemetry.sdk.metrics import MeterProvider
+# from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+# from opentelemetry.exporter.prometheus import PrometheusMetricReader
+# from opentelemetry.instrumentation.django import DjangoInstrumentor
+# from opentelemetry.sdk.resources import Resource
 
-def setup_otel_metrics():
-    # Configurar o provedor de métricas
-    metric_reader = PrometheusMetricReader()
+# def setup_otel_metrics():
+#     # Configurar o provedor de métricas
+#     metric_reader = PrometheusMetricReader()
     
-    metrics.set_meter_provider(
-        MeterProvider(
-            metric_readers=[metric_reader],
-            resource=Resource.create({"service.name": "django-app"})
-        )
-    )
+#     metrics.set_meter_provider(
+#         MeterProvider(
+#             metric_readers=[metric_reader],
+#             resource=Resource.create({"service.name": "django-app"})
+#         )
+#     )
     
-    # Instrumentar o Django
-    DjangoInstrumentor().instrument()
+#     # Instrumentar o Django
+#     DjangoInstrumentor().instrument()
